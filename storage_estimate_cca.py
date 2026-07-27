16360fcd81fe51f09b715c776c25c6639e1008f7
Jesus Fernandez <jesusff@users.noreply.github.com>
2026-07-27 19:59:04 +0200

    Add data request and storage estimate script
    Based on https://github.com/WCRP-CORDEX/simulation-status/blob/main/scripts/storage_estimate.py

---
+++ storage_estimate_cca.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+from icecream import ic
+
+precision_factor = 4 # float
+compression_factor = 0.6
+bytes_to_TB = 1.0e-12
+priorities = ['CORE', 'TIER1', 'TIER2']
+statuses = ['published', 'completed', 'running', 'planned']
+
+# Number of time records per year
+frequency_factor = {'mon': 12, 'day': 365, '6hr': 365*4, '1hr': 365*24}
+
+# Number of years depending on the experiment. Minimal periods considered here
+# as the evaluation can be extended beyond 2020 and the historical could start
+# in 1951. See https://cordex.org/wp-content/uploads/2021/05/CORDEX-CMIP6_exp_design_RCM.pdf
+experiment_factor = {'evaluation': 2020-1980+1, 'historical': 2014-1961+1}
+experiment_factor_default = 2100-2015+1
+
+domains = pd.read_csv(
+  'https://raw.githubusercontent.com/WCRP-CORDEX/domain-tables/refs/heads/main/CORDEX-CMIP6_grids.csv',
+  usecols=['domain_id', 'n_longitude', 'n_latitude']
+)
+domains['ngridcells'] = domains['n_longitude'] * domains['n_latitude']
+ngridcells = (domains
+  .drop(columns=['n_longitude', 'n_latitude'])
+  .set_index('domain_id')
+  .to_dict()
+  .get('ngridcells')
+)
+# Some fixes for missing domains
+ngridcells['AUS-20i'] = ngridcells['AUS-25'] #!!
+ngridcells['MENA-25'] = ngridcells['MNA-25']
+ngridcells['MED-25'] = ngridcells['MED-12']/4
+ngridcells['SEA-12'] = ngridcells['SEA-25']*4
+ngridcells['AFR-18'] = ngridcells['AFR-25']*2
+ngridcells['WAS-18'] = ngridcells['WAS-25']*2
+ngridcells['CAM-18'] = ngridcells['CAM-25']*2
+
+plans = pd.read_csv(
+  'https://raw.githubusercontent.com/WCRP-CORDEX/simulation-status/refs/heads/main/CMIP6_downscaling_plans.csv',
+  usecols=['domain_id', 'institution_id', 'driving_experiment_id', 'status', 'comments']
+).query('status in @statuses')
+
+# Filter out ESD plans as their output will likely contain very
+# limited variables and maximum at daily frequency
+plans = plans[~plans['comments'].str.contains('#ESD', na=False)]
+
+# /!\ Only CORE
+plans = plans[plans['comments'].str.contains('#CORDEX-CORE', na=False)]
+
+simulation_count = plans.pivot_table(
+  index = 'domain_id',
+  columns= 'driving_experiment_id',
+  aggfunc='size',
+  fill_value = 0
+).drop(columns = ['TBD','no plans','selected'], errors='ignore')
+ic(simulation_count)
+
+dreq = pd.read_csv(
+  'dreq_CCAnalyses.csv'
+)
+dreq = dreq[dreq['n_analyses'] > 0]
+
+variable_count = dreq.pivot_table(
+  index = 'frequency',
+  columns= 'priority',
+  aggfunc='size',
+  fill_value = 0
+)
+ic(variable_count)
+
+freq_factor = variable_count.index.map(lambda x: frequency_factor.get(x, 0))
+variable_records_per_yr = (variable_count
+  .mul(freq_factor, axis=0)
+)
+ic(variable_records_per_yr)
+
+# Just the these variable records
+print(f'/!\ Considering {priorities} variables')
+nrecords_factor = variable_records_per_yr[priorities].values.sum()
+
+ngridcell_factor = simulation_count.index.map(lambda x: ngridcells.get(str(x)))
+exp_factor = simulation_count.columns.map(lambda x: experiment_factor.get(x, experiment_factor_default))
+
+size_TB = (simulation_count
+  .mul(ngridcell_factor, axis=0)
+  .mul(exp_factor, axis=1)
+  .mul(nrecords_factor)
+  .mul(precision_factor)
+  .mul(compression_factor)
+  .mul(bytes_to_TB)
+  .round(1)
+)
+ic(size_TB)
+
+ic(size_TB.T.sum())
+
+print(f'/!\ Considering status {statuses}')
+print(f'Total CORDEX-CMIP6 estimated size is: {np.nansum(size_TB.values):.0f} TB')
